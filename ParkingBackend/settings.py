413fc51d1bfea7946dd9dcdf6472d5019331cae5
dip_sarker <sarkerdip005@gmail.com>
2025-02-19 12:43:25 +0600

    addsupabase

--- ParkingBackend/settings.py
+++ ParkingBackend/settings.py
@@ -115,27 +115,27 @@ WSGI_APPLICATION = 'ParkingBackend.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE':"django.db.backends.postgresql",
-#         'NAME':"postgres",
-#         'USER':os.getenv("db_user"),
-#         'PASSWORD':os.getenv("db_password"),
-#         'HOST':"aws-0-us-west-1.pooler.supabase.com",
-#         'PORT':5432,
-#         'OPTIONS': {
-#             'sslmode': 'require'  
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE':"django.db.backends.postgresql",
+        'NAME':"postgres",
+        'USER':os.getenv("db_user"),
+        'PASSWORD':os.getenv("db_password"),
+        'HOST':"aws-0-us-west-1.pooler.supabase.com",
+        'PORT':6543,
+        'OPTIONS': {
+            'sslmode': 'require'  
+        },
+    }
+}
 
 
 
